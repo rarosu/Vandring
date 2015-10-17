a277b9a01298640e03775607fb95565f5f39b91f
Lars Woxberg | Rarosu <lars.woxberg@gmail.com>
2015-10-17 08:58:55 +0200

    Initial commit.
    Bare MVC project added.

---
+++ Vandring.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vandring.Web
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.adx/{*pathInfo}");
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
